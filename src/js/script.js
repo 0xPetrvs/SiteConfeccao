48637be9e1c87cad9158465e4fa2c85feafc3d16
The Henrique <rique.souza06@icloud.com>
2025-05-01 00:00:48 -0300

    feat: hamburguer

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,36 @@
+class MobileNavbar{
+    constructor(mobileMenu,navList,navLinks){
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelector(navLinks);
+        this.activeClass="active"
+        this.handleClick = this.handleClick.bind(this)
+    }
+    animateLinks(){
+        this.navLinks.forEach((Link,index) => {
+            Link.style.animation
+            ? (link.style.animation="")
+            : (link.style.aniamtion=`navLinkFade 0.5 ease forwards 0.3`);});
+        }
+    handleClick(){
+        this.navList.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+    addClickEvent(){
+        this.mobileMenu.addEventListener("click",this.handleClick);
+    }
+    init(){
+        if(this.mobileMenu){
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
 
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+);
+
+mobileNavbar.init()
