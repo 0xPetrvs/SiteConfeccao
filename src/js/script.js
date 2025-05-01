db7190cce8e5e5fcfd7c33c164024ebadb353464
The Henrique <rique.souza06@icloud.com>
2025-05-01 17:36:11 -0300

    feat: all finish

--- src/js/script.js
+++ src/js/script.js
@@ -1,36 +1,44 @@
-class MobileNavbar{
-    constructor(mobileMenu,navList,navLinks){
-        this.mobileMenu = document.querySelector(mobileMenu);
-        this.navList = document.querySelector(navList);
-        this.navLinks = document.querySelector(navLinks);
-        this.activeClass="active"
-        this.handleClick = this.handleClick.bind(this)
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
     }
-    animateLinks(){
-        this.navLinks.forEach((Link,index) => {
-            Link.style.animation
-            ? (link.style.animation="")
-            : (link.style.aniamtion=`navLinkFade 0.5 ease forwards 0.3`);});
-        }
-    handleClick(){
-        this.navList.classList.toggle(this.activeClass);
-        this.animateLinks();
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
     }
-    addClickEvent(){
-        this.mobileMenu.addEventListener("click",this.handleClick);
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
     }
-    init(){
-        if(this.mobileMenu){
-            this.addClickEvent();
-        }
-        return this;
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
     }
-}
-
-const mobileNavbar = new MobileNavbar(
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
     ".mobile-menu",
     ".nav-list",
     ".nav-list li",
-);
-
-mobileNavbar.init()
+  );
+  mobileNavbar.init();
